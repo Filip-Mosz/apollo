01289106c44981d5f743329b25e39fda4ac77116
Filip M <filip.moszczynski@onet.pl>
2020-10-25 14:24:46 +0100

    > D5.0 - ListResponse

---
+++ src/main/java/com/filipmoszczynski/core/pagination/ListResponse.java
@@ -0,0 +1,26 @@
+package com.filipmoszczynski.core.pagination;
+
+import java.util.List;
+
+public class ListResponse<T> {
+
+    private final Metadata metadata;
+    private final List<T> content;
+
+    public ListResponse(List<T> content,
+                        int totalPages,
+                        long totalElements,
+                        int size,
+                        int page){
+        this.metadata = new Metadata(totalPages,totalElements,size,page);
+        this.content = content;
+    }
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    public List<T> getContent() {
+        return content;
+    }
+}
